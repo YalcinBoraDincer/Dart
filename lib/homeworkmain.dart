e93162d0d01e5f90bb1585fc8a71fd8d784aca92
Bora <40895613+YalcinBoraDincer@users.noreply.github.com>
2023-03-22 12:58:15 +0300

    OOP

---
+++ lib/homeworkmain.dart
@@ -0,0 +1,20 @@
+import 'package:objectoop/homeWork.dart';
+
+void main(){
+  var hw1=homework();
+
+
+
+
+
+
+
+  int gelenMaas=hw1.salaryCalc(30);
+  print("Maas : $gelenMaas");
+
+  hw1.wordChecker("ankara", "a");
+
+
+}
+
+
